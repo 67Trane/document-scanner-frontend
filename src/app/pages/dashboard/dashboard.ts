ece600bedcf2ff9df3b6219cd57293fd4e21cb9f
Trane <Trane_67@hotmail.de>
2025-12-04 00:02:33 +0100

    change colors

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CustomerSearch } from '../../components/customer-search/customer-search';
 import { CustomerList } from '../../components/customer-list/customer-list';
 import { DocumentList } from '../../components/document-list/document-list';
+
+type SidebarSection = 'overview' | 'customers' | 'documents' | 'settings';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,5 +14,9 @@ import { DocumentList } from '../../components/document-list/document-list';
 })
 export class Dashboard {
   year = new Date().getFullYear();
+  activeSection = signal<SidebarSection>('overview');
 
+  setActive(section: SidebarSection) {
+    this.activeSection.set(section);
+  }
 }
